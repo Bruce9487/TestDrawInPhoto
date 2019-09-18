f49b4e7ae57dadea9183b05f8a115f26dd4fa814
APP技術部-陳冠志 <bruce@mitake.com.tw>
2019-09-18 18:37:26 +0800

    Test

--- TestDrawInPhoto/ViewController.swift
+++ TestDrawInPhoto/ViewController.swift
@@ -8,13 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        
     }
 
+    @IBAction func cameraBtnPressed(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .camera
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
+    }
 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+    }
+    
 }
+
 
